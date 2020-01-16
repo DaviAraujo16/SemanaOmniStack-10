3774930ea18547e21bce0aab91cdae0d1a724d5e
Davi Araújo <davirafael.araujo@gmail.com>
2020-01-15 23:45:21 -0300

    Versão onde foi desenvolvido o Front-End Web utilizando ReactJS

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import api from './services/api';
+
+import './global.css';
+import './app.css';
+import './sidebar.css';
+import './main.css';
+
+import DevForm from './components/DevForm/index';
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(()=>{
+    async function loadDevs(){
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, [])
+
+  async function handleAddDev(data){
+    const response = await api.post('/devs', data)
+    setDevs([...devs, response.data]);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev}/>
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev =>(
+            <li key={dev._id} className="dev-item">
+                <header>
+                    <img src={dev.avatar_url} alt={dev.name}/>
+                    <div className="user-info">
+                        <strong>{dev.name}</strong>
+                        <span>{dev.techs.join(', ')}</span>
+                    </div>
+                </header>
+                <p>{dev.bio}</p>
+                <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
+
 
 export default App;
