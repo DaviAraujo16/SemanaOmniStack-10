28ee130226fb8eb88aec4b970d05745d833e3377
Davi Araújo <43482358+DaviAraujo16@users.noreply.github.com>
2020-01-21 09:53:53 -0300

    retirada de algumas informações...

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,6 @@ const routes = require('./routes');
 const cors = require('cors');
 
 const app = express();
-
-mongoose.connect('mongodb+srv://davi:18175157@cluster0-bfzap.mongodb.net/week10?retryWrites=true&w=majority', {
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-});
-
 app.use(cors());
 app.use(express.json())
 app.use(routes);
